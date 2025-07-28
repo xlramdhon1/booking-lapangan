8e415fbb34e0f04c830c708b6a4dac18cf521b91
xlramdhon1 <ramdhon1.ssss@gmail.com>
2025-07-28 13:31:26 +0700

    FINAL V2

--- app/Views/dashboard/booking/booking_list.php
+++ app/Views/dashboard/booking/booking_list.php
@@ -2,7 +2,7 @@
 <?= $this->section('content') ?>
 <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
 
-<h2 class="text-2xl font-bold text-blue-700 mb-6">Daftar Booking</h2>
+<h2 class="text-2xl font-bold text-blue-700 mb-6">Daftar Ahsiap</h2>
 
 <?php if(session()->getFlashdata('success')): ?>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
